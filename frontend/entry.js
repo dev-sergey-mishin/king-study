2e028ff99c0eda41c2d01abf32d637ccbc983d4d
sergey <sergey.mishin.71@gmail.com>
2017-10-22 01:46:14 +0300

    немного оптимизации

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,10 +1,22 @@
 require('./less/import.less');
 
 let loader = require('./js/loader.js');
-loader.loadContent().then(
-    () => {
-        require('./js/header.js');
-        require('./js/screen-2.js');
-        require('./js/slider.js');
-    }
-);
+
+$(document).ready(() => {
+    loader.loadContent().then(
+        () => {
+            require('./js/header.js');
+            require('./js/screen-2.js');
+            require('./js/slider.js');
+
+            try {
+                WebFont.load({
+                    google: { families: ['Open+Sans:400,600,800'] }
+                });
+            } catch (err) {
+                console.log('WebFont not found');
+            }
+
+        }
+    );
+});
